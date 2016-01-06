3c0609b9cdb445fb652531b75784a405d61dace4
Roshane De Silva <roshaneis@gmail.com>
2016-01-06 20:44:46 +0530

    Linkedin Login
    Login to the app

---
+++ app/src/main/java/com/apps/roshanedesilva/cvcreator/Config.java
@@ -0,0 +1,16 @@
+package com.apps.roshanedesilva.cvcreator;/**
+ * Created by Roshane De Silva on 05/01/2016.
+ */
+
+/**
+ * Created by Roshane De Silva on 05/01/2016.
+ */
+public class Config {
+    public static String LINKEDIN_CONSUMER_KEY = "751sc18ik6g4ix";
+    public static String LINKEDIN_CONSUMER_SECRET = "1Zinj2P0sLDVphb0";
+    public static String scopeParams = "rw_nus+r_basicprofile";
+
+    public static String OAUTH_CALLBACK_SCHEME = "x-oauthflow-linkedin";
+    public static String OAUTH_CALLBACK_HOST = "callback";
+    public static String OAUTH_CALLBACK_URL = OAUTH_CALLBACK_SCHEME + "://" + OAUTH_CALLBACK_HOST;
+}
